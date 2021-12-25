9a84aaa3f833e18ed6c3482f3939a079663d5b7f
Hung-Yu Ke <tt960945@gmail.com>
2021-12-26 01:48:39 +0800

    bug fix 對應資料庫進行調整

--- PCReDive_Hatsune/Module/auto_clear.py
+++ PCReDive_Hatsune/Module/auto_clear.py
@@ -1,7 +1,7 @@
 import Discord_client
 import Module.DB_control
-#import Module.Offset_manager
 import Module.Update
+import Module.report_update
 
 
 async def auto_clear():
@@ -13,20 +13,21 @@ async def auto_clear():
     sql = "DELETE FROM princess_connect_hatsune.knifes"
     cursor.execute(sql)
 
-    # 重設boss次序
-    sql = "UPDATE princess_connect_hatsune.sl_info SET sl_use='0'"
+    # 重設week到第一週
+    sql = "UPDATE princess_connect_hatsune.group SET now_week='1'"
     cursor.execute(sql)
-    connection.commit()
 
     # 更新所有戰隊刀表
-    sql = "SELECT server_id, sign_channel_id, action_channel_id, report_channel_id FROM princess_connect.hatsune.group"
+    sql = "SELECT server_id, sign_channel_id, action_channel_id, action_message_id, reserved_message_id, report_message_id FROM princess_connect_hatsune.group"
     cursor.execute(sql)
     rows = cursor.fetchall()  
     for row in rows:
       server_id = row[0]
       sign_channel_id = row[1]
       action_channel_id = row[2]
-      report_channel_id = row[3]
+      action_message_id = row[3]
+      reserved_message_id = row[4]
+      report_message_id = row[5]
 
       
       guild = Discord_client.client.get_guild(server_id)
@@ -40,13 +41,11 @@ async def auto_clear():
           # 取得活動頻道
           try:
             channel = guild.get_channel(action_channel_id)
-            # 刷新活動資訊 TODO
-            # 刷新出刀資訊 TODO
-            # Module.Offset_manager.AutoOffset(connection, server_id, group_serial) # 自動周目控制
-            # await Module.Update.Update(message_obj, server_id, group_serial) # 更新刀表
-            message_obj.edit(content = '已完成戰前重置流程!')
+            await Module.Update.Update(message_obj, server_id) # 更新刀表  
+            await Module.report_update.report_update(message_obj, server_id)
+            await message_obj.edit(content = '已完成戰前重置流程!')
           except:
-            print('伺服器:' + str(server_id) + ', 編號' + '頻道:' + str(action_channel_id) + '刀表頻道不存在!')
+            print('伺服器:' + str(server_id) + ', 編號' + '頻道:' + str(action_channel_id) + '活動頻道不存在!')
         except:
           print('伺服器:' + str(server_id) + ', 編號' + '頻道:' + str(sign_channel_id) + '報刀頻道不存在!')
       else: # 清除不存在的戰隊資料
@@ -58,7 +57,7 @@ async def auto_clear():
         data = (server_id, )
         cursor.execute(sql, data)
 
-        sql = "DELETE FROM princess_connect_hatsune.sl_info where server_id = ?"
+        sql = "DELETE FROM princess_connect_hatsune.members where server_id = ?"
         data = (server_id, )
         cursor.execute(sql, data)
 
